f0f9cae09cdab6230ba13468a87746d527fd3eba
ken.lee <ken.lee@linecorp.com>
2020-02-04 21:48:49 +0900

    Add a z-index

--- show-touch.js
+++ show-touch.js
@@ -5,6 +5,7 @@ $point.style.borderRadius = '50%';
 $point.style.background = 'red';
 $point.style.position = 'fixed';
 $point.style.transform = 'translate(-50%, -50%)';
+$point.style.zIndex = '99999999999999999';
 document.body.appendChild($point);
 
 document.body.addEventListener('click', e => {
