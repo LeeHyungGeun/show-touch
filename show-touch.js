d69ecc6b766c1ed0682bfeec59065af59d45fce3
ken.lee <ken.lee@linecorp.com>
2020-02-04 21:42:39 +0900

    Init a project

---
+++ show-touch.js
@@ -0,0 +1,15 @@
+const $point = document.createElement('div');
+$point.id = 'mtouch-point';
+$point.style.padding = '5px';
+$point.style.borderRadius = '50%';
+$point.style.background = 'red';
+$point.style.position = 'fixed';
+$point.style.transform = 'translate(-50%, -50%)';
+document.body.appendChild($point);
+
+document.body.addEventListener('click', e => {
+  const { pageX, pageY } = e;
+
+  $point.style.left = pageX + 'px';
+  $point.style.top = pageY + 'px';
+});
